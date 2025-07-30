a2da95e9f5ce2b2cc4857bcbf945fd170a9d7b7e
khamzaev <khamzaew.j@gmail.com>
2025-07-30 20:58:36 +0300

    Integrated Core Data

---
+++ MyLocations/CoreData/Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by khamzaev on 30.07.2025.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
