398126d18440f1f632a691e0d8408b5ccc2bb50b
khamzaev <khamzaew.j@gmail.com>
2025-07-31 22:25:39 +0300

    LocationsViewController

---
+++ MyLocations/Controllers/LocationsViewController.swift
@@ -0,0 +1,80 @@
+//
+//  LocationsViewController.swift
+//  MyLocations
+//
+//  Created by khamzaev on 31.07.2025.
+//
+
+import UIKit
+import CoreData
+import CoreLocation
+
+
+class LocationsViewController: UITableViewController {
+    var managedObjectContext: NSManagedObjectContext!
+    var locations = [Location]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fetchRequest = NSFetchRequest<Location>()
+        
+        let entity = Location.entity()
+        fetchRequest.entity = entity
+        
+        let sortDescriptor = NSSortDescriptor(
+            key: "date",
+            ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        do {
+            locations = try managedObjectContext.fetch(fetchRequest)
+        } catch {
+            fatalCoreDataError(error)
+        }
+    }
+    
+    
+    
+    // MARK: - Table View Delegates
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return locations.count
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "LocationCell",
+            for: indexPath
+        )
+        
+        let location = locations[indexPath.row]
+        
+        let descriptionLabel = cell.viewWithTag(100) as! UILabel
+        descriptionLabel.text = location.locationDescription
+        
+        let addressLabel = cell.viewWithTag(101) as! UILabel
+        if let placemark = location.placemark {
+            var text = ""
+            if let tmp = placemark.subThoroughfare {
+                text += tmp + " "
+            }
+            if let tmp = placemark.thoroughfare {
+                text += tmp + ", "
+            }
+            if let tmp = placemark.locality {
+                text += tmp
+            }
+            addressLabel.text = text
+        } else {
+            addressLabel.text = ""
+        }
+        return cell
+    }
+    
+}
