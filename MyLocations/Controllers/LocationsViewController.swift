b0aefd9a42b7662c9fa18fcedc917362f1f301e3
khamzaev <khamzaew.j@gmail.com>
2025-08-02 12:40:09 +0300

    Swipe for delete,Mass editing

--- MyLocations/Controllers/LocationsViewController.swift
+++ MyLocations/Controllers/LocationsViewController.swift
@@ -41,6 +41,8 @@ class LocationsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = editButtonItem
+        
         performFetch()
     }
     
@@ -69,6 +71,22 @@ class LocationsViewController: UITableViewController {
         
         return cell
         
+    }
+    
+    override func tableView (
+        _ tableView: UITableView,
+        commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath
+    ) {
+        if editingStyle == .delete {
+            let location = fetchedResultsController.object(at: indexPath)
+            managedObjectContext.delete(location)
+            do {
+                try managedObjectContext.save()
+            } catch {
+                fatalCoreDataError(error)
+            }
+        }
     }
     
     
